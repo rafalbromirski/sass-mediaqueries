53e1b21f3a958467a9553e34933cb81b3fb9b4c3
Rafal Bromirski <rafal.bromirski@gmail.com>
2013-11-14 22:48:29 +0100

    version 1.3

--- demo/sass/config.rb
+++ demo/sass/config.rb
@@ -1,8 +1,6 @@
 # Get the directory that this configuration file exists in
 dir = File.dirname(__FILE__)
 
-# load File.join(dir, '..', 'themes')
-
 # Compass configurations
 sass_path    = dir
 css_path     = File.join(dir, "..", "css")
