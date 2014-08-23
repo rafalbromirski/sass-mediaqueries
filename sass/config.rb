b9aef70f552e2fa538d9212d00b88f35eab5bea6
Rafal Bromirski <rafal.bromirski@gmail.com>
2014-08-23 18:09:17 +0200

    new page

--- sass/config.rb
+++ sass/config.rb
@@ -1,10 +1,8 @@
 # Get the directory that this configuration file exists in
 dir = File.dirname(__FILE__)
 
-load File.join(dir, 'libs')
-
 # Compass configurations
 sass_path    = dir
-css_path     = File.join(dir, "..", "stylesheets")
+css_path     = File.join(dir, "..", "css")
 environment  = :production
 output_style = :expanded
